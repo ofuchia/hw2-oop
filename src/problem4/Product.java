f3ca81beb3dd26e63331f57aaa3c88b0d14220a0
ofuchia <o.akpom@tcu.edu>
2023-09-30 21:42:08 -0500

    Starting 4

--- src/problem4/Product.java
+++ src/problem4/Product.java
@@ -3,41 +3,12 @@ package problem4;
 import java.math.BigDecimal;
 
 public class Product {
-    private BigDecimal bidAmount;
-    private String productName;
-    private BigDecimal initalPrice;
+    String productName;
+    BigDecimal initialPrice;
+    BigDecimal newBidPrice;
 
-    public Product(String productName, BigDecimal initalPrice) {
+    public Product(String productName, BigDecimal initialPrice) {
         this.productName = productName;
-        this.initalPrice = initalPrice;
+        this.initialPrice = initialPrice;
     }
-
-    public BigDecimal getBidAmount() {
-        return bidAmount;
-    }
-
-    public void setBidAmount(BigDecimal bidAmount) {
-        this.bidAmount = bidAmount;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public BigDecimal getInitalPrice() {
-        return initalPrice;
-    }
-
-    public void setInitalPrice(BigDecimal initalPrice) {
-        this.initalPrice = initalPrice;
-    }
-
-    //should be same product name as above.
-//    public void subscribeProduct(String productName, BigDecimal newBidPrice){
-//        this.productName = productName.get
-//    }
 }
